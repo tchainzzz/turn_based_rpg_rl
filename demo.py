ef545f079c2e89857615c4230be7c6620eccaa3d
tchainzzz <tchang97@stanford.edu>
2021-07-12 14:46:55 -0600

    multi-player parties

--- demo.py
+++ demo.py
@@ -4,12 +4,12 @@ from copy import copy
 import random
 
 if __name__ == '__main__':
-    env = TurnBasedRPGEnv(['hero'], ally_file="allies.csv", enemy_file="enemies.csv", action_file="actions.csv")
-    curr_player = env.state.current_player()
+    env = TurnBasedRPGEnv(['hero', 'test_ally'], ally_file="allies.csv", enemy_file="enemies.csv", action_file="actions.csv")
 
     env.reset()
     is_terminal = False
     while not is_terminal:
+        curr_player = env.state.current_player()
         action_names = curr_player.get_legal_actions(env.entity_bank)
         actions = [env.entity_bank.create_action(a, curr_player) for a in action_names]
         action = random.choice(actions)
